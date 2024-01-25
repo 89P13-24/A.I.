229cc1b4df0e78fb21e6aa88ad091a5ef7500d48
Ishan Gupta <134151658+89P13-24@users.noreply.github.com>
2024-01-25 09:45:52 +0530

    Update manhattan.cpp

--- 8-blocks/manhattan.cpp
+++ 8-blocks/manhattan.cpp
@@ -10,16 +10,7 @@ public :
     }
 };
 
-vector<int> transform_(int n,vector<vector<int>> v){
-    vector<int> res(n*n);
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            res[i*n+j] = v[i][j];
-        }
-    }
-    return res;
-}
+vector<int> transform_(int n,vector<vector<int>> v);
 pair<int,int> find_index(int n,int index,vector<vector<int>> &s);
 int manhattan(int n,vector<int> &s,vector<int> &g);
 int solvable_odd(vector<int> &v,vector<int> &g);
@@ -28,81 +19,46 @@ int No_misplaced_tile(vector<int> &h,vector<int> &g);
 vector<vector<int>> neighbors(int n,vector<int> &s);
 void print_2D(int n,vector<int> &v);
 void print_solution(vector<vector<int>> &v,int n);
-int closed(vector<int> &v,vector<vector<int>> &soln){
-    int flag = 10;
-    for(auto i : soln){
-        flag = min(flag,No_misplaced_tile(v,i));
-    }
-    return flag;
-}
+int closed(vector<int> &v,vector<vector<int>> &soln);
+
 
 int main()
 {
-	//cout<<"Enter 0 for empty space"<<"\n";
-	//vector<int> g{1,2,3,8,0,4,7,6,5};
+    clock_t start,endt;
 	vector<int> v{4,6,5,2,7,8,1,0,3};
     vector<int> g{1,2,3,4,5,6,7,8,0};
-	//cout<<"Enter start state"<<"\n";,
-	//for(int i=0;i<9;i++) cin>>v[i];
-	//cout<<"Enter goal state"<<"\n";
+    //vector<int> v{0,1,2,3,4,5,6,7,8};
 	int cc = 0;
-//
-//	for(int i=0;i<9;i++){
-//	    v[i] = i;
-//	    g[i] = i+1;
-//	}
-//	g[8] = 0;
-	//g1 = v;
 	vector<vector<int>> start_states(10,vector<int>(9)) ;
-//	vector<vector<int>> goal_states(10,vector<int>(9)) ;
 	while(cc<10){
         //shuffle(g.begin(),g.end(),default_random_engine(time(0)));
         shuffle(v.begin(),v.end(),default_random_engine(time(0)));
         start_states[cc] = v;
-        //goal_states[c] = g;
         cc++;
 	}
-//	int ans = 0;
-//    print_2D(3,v);
-//    print_2D(3,g);
-//    cout<<manhattan(3,v,g)<<endl;
-//    return 0;
+
     //shuffle(v.begin(),v.end(),default_random_engine(time(0)));
 
-//	for(int i=0;i<10;i++){
-//        print_state(start_states[i]);
-//        print_state(g1);
-//        cout<<No_misplaced_tile(start_states[i],g1)<<endl;
-//        if(solvable_odd(start_states[i],goal_states[2]))
-//            ans++;
-//	}
-//	print_state(g1);
-//	swap(g1[0],g1[1]);
-//	print_state(g1);
-	//cout<<ans<<endl;
-    print_state(start_states[1]);
+    print_state(start_states[0]);
     print_state(g);
-    if(!solvable_odd(v,g)){
+    if(!solvable_odd(start_states[0],g)){
         cout<<"NOT SOLVABLE"<<endl;
         return 0;
     }
     else{
         cout<<"SOLVABLE"<<endl;
    }
-//	state* st = new state;
-//	st->s = v;
-//	st->distance = No_misplaced_tile(v,g);
+    start = clock();
     priority_queue<state,vector<state>,greater<state>> q;
     int c =0;
     state st,temp;
-    st.s = start_states[1];
+    st.s = start_states[0];
     st.distance = manhattan(3,v,g);
     q.push(st);
-	//queue<vector<int>> q;
-	//q.push(v);
+
 	vector<vector<int>> soln;
 	vector<vector<int>> done;
-	//vector<int> temp;
+
 
 	while(!q.empty() && c<100000){
         temp = q.top();
@@ -118,10 +74,9 @@ int main()
         done.push_back(temp.s);
         vector<vector<int>> ngh = neighbors(3,temp.s);
         for(auto i : ngh){
-//            temp->s = i;
             if(closed(i,done) == 0)
                continue;
-//            temp->distance = No_misplaced_tile(i,g);
+
             state z;
             z.s = i;
             z.distance = manhattan(3,i,g);
@@ -129,9 +84,18 @@ int main()
         }
         c++;
 	}
+	endt = clock();
+	double time_taken = double(endt - start) / double(CLOCKS_PER_SEC);
+
     print_solution(soln,3);
-    cout<<c<<endl;
+    cout<<"No. of states visited : "<<c<<endl;
+    cout<<"No. of nodes removed from the Frontier : "<<q.size()-c<<endl;
+     cout << "Time taken by program is : " << fixed
+         << time_taken << setprecision(5);
+    cout << " sec " << endl;
 }
+
+                                        /* ......Definitions of different functions......*/
 
 int manhattan(int n,vector<int> &s,vector<int> &g){
     vector<vector<int>> st(n,vector<int>(n)),tar(n,vector<int>(n));
@@ -261,5 +225,24 @@ void print_solution(vector<vector<int>> &v,int n){
     for(auto i:v){
         print_2D(n,i);
     }
+}
+
+
+vector<int> transform_(int n,vector<vector<int>> v){
+    vector<int> res(n*n);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            res[i*n+j] = v[i][j];
+        }
+    }
+    return res;
+}
+int closed(vector<int> &v,vector<vector<int>> &soln){
+    int flag = 10;
+    for(auto i : soln){
+        flag = min(flag,No_misplaced_tile(v,i));
+    }
+    return flag;
 }
 
